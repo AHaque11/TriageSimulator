435a8887603d0c39c0c6e83aee5a60f6c27d7800
AHaque11 <84865759+AHaque11@users.noreply.github.com>
2022-09-11 21:17:18 -0400

    Add files via upload

---
+++ TriageDemo.java
@@ -0,0 +1,44 @@
+// Arefa Haque
+// CIS 2353
+// Spring 2022
+// Prof. John P. Baugh
+package project4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class TriageDemo {
+
+    public static void main(String[] args) {
+        
+        TriageSimulator waitingRoom = new TriageSimulator();
+        
+        try{
+            Scanner inFile = new Scanner(new File("patient_list.txt"));
+            
+            while(inFile.hasNextLine()){ 
+                String fileLine = inFile.nextLine();
+                waitingRoom.add(fileLine);
+            }
+            
+            inFile.close();     
+        }
+        catch(FileNotFoundException ex){
+            System.out.println("ERROR: Could not open file. Please reopen the "
+                    + "program and try again.\n");
+            System.exit(0);
+        }
+        
+        
+        System.out.println("\nTRIAGE ORDER: ");
+        
+        while(!waitingRoom.isEmpty()){
+            waitingRoom.remove();
+        }
+        
+        System.out.println("\n");
+        
+    }
+}
