0d04d8e873b6d7c4ab23d71fa241fd6303d5a1f8
AHaque11 <84865759+AHaque11@users.noreply.github.com>
2022-09-12 14:08:26 -0400

    Add files via upload

---
+++ LinkedQueue.java
@@ -0,0 +1,100 @@
+
+package Triage;
+
+
+
+interface QueueInterface<T> {
+    
+    public void enQueue(T newEntry);
+    public T deQueue();
+    public Boolean isEmpty();
+    public void clear();
+}
+
+
+
+public class LinkedQueue<T> implements QueueInterface<T> {
+  
+    private class Node{
+        private T data;
+        private Node next;
+        
+        public Node(T data, Node next){
+            
+            this.data = data;
+            this.next = next;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }  
+    }
+    
+    
+    
+    private Node firstNode;
+    private Node lastNode;
+    
+    public LinkedQueue(){
+        
+        firstNode = null;
+        lastNode = null;
+    }
+    
+    @Override
+    public void enQueue(T newEntry) {        
+        
+        Node newNode = new Node(newEntry, null);
+        
+        if(isEmpty()){
+            firstNode = newNode;
+        }
+        else{
+            lastNode.setNext(newNode);
+        }
+        
+        lastNode = newNode;
+    }
+    
+    @Override
+    public T deQueue() {
+       
+        T front = firstNode.getData();
+        
+        assert firstNode != null;
+        firstNode.setData(null);
+        firstNode = firstNode.getNext();
+        
+        if(firstNode == null){
+            lastNode = null;
+        }
+ 
+        return front;
+    }
+     
+    @Override
+    public Boolean isEmpty() {
+        
+        return (firstNode == null) && (lastNode == null);
+    }
+
+    @Override
+    public void clear() {
+        
+        firstNode = null;
+        lastNode = null;
+    }
+    
+}
---
+++ TriageDemo.java
@@ -0,0 +1,41 @@
+
+package Triage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class TriageDemo {
+
+    public static void main(String[] args) {
+        
+        TriageSimulator waitingRoom = new TriageSimulator();
+        
+        try{
+            Scanner inFile = new Scanner(new File("patient_list.txt"));
+            
+            while(inFile.hasNextLine()){ 
+                String fileLine = inFile.nextLine(); //Read one line at a time
+                waitingRoom.add(fileLine); //Add info from each line to the queues
+            }
+            
+            inFile.close();     
+        }
+        catch(FileNotFoundException ex){
+            System.out.println("ERROR: Could not open file. Please reopen the "
+                    + "program and try again.\n");
+            System.exit(0);
+        }
+        
+        
+        System.out.println("\nTRIAGE ORDER: ");
+        
+        while(!waitingRoom.isEmpty()){
+            waitingRoom.remove();
+        }
+        
+        System.out.println("\n");
+        
+    }
+}
